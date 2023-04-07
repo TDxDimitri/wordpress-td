fb78a26a9dba0b860309422601f75fc33fc21a70
TDxDimitri <talleu.dimitri@outlook.fr>
2023-04-07 11:58:55 +0200

    deuxieme version

--- wp-content/themes/montheme/functions.php
+++ wp-content/themes/montheme/functions.php
@@ -5,6 +5,7 @@ function montheme_supports() {
     add_theme_support('post-thumbnails');
     add_theme_support('menu');
     register_nav_menu('header', 'En tête du menu');
+    register_nav_menu('footer', 'Pied de page');
 }
 function montheme_register_assets () {
     wp_register_style('bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css');
@@ -15,10 +16,27 @@ function montheme_register_assets () {
     wp_enqueue_style('bootstrap');
     wp_enqueue_script('bootstrap');
 }
+function montheme_title_separator()
+{
+    return '|';
+}
 
+function montheme_menu_class($classes)
+{
+    $classes[] = 'nav-item';
+    return $classes;
+}
 
+function montheme_menu_link_class($attrs)
+{
+    $attrs['class'] = 'nav-link';
+    return $attrs;
+}
 
 
 add_action('after_setup_theme', 'montheme_supports');
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
+add_filter('document_title_separator', 'montheme_title_separator');
+add_filter('nav_menu_css_class', 'montheme_menu_class');
+add_filter('nav_menu_link_attributes', 'montheme_menu_link_class');
 ?>
--- wp-content/themes/montheme/header.php
+++ wp-content/themes/montheme/header.php
@@ -1,22 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <?php wp_head() ?>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Top-Ciné</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <?php wp_nav_menu(['theme_location' => 'header', 'container' => false, 'menu_class' => 'navbar-nav mr-auto']) ?>
-    <!--   
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <?php wp_head() ?>
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary mb-4">
+    <div class="container">
+      <a class="navbar-brand" href="#"><?php bloginfo('name') ?></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedCntent">
+        <?php wp_nav_menu([
+          'theme_location' => 'header',
+          'container' => false,
+          'menu_class' => 'navbar-nav mr-auto'
+        ]) ?>
+        <!--   
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
@@ -40,10 +47,11 @@
       </li>
     </ul>
 -->
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-    <div class="container d-flex">
+
+      </div>
+    </div>
+  </nav>
+
+  <?= get_search_form() ?>
+
+  <div class="container">
